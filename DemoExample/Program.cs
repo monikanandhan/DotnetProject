cc28068df129f467402074841e8829a2717d63d1
Monica <monica.a@kgisl.com>
2023-03-01 10:22:56 +0530

    Add existing project files to Git

---
+++ DemoExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
