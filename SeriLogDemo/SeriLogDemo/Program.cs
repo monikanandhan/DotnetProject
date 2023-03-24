e80b50d0b61fb70c5edb23170d6ed171a8909a8b
monikanandhan <109712274+monikanandhan@users.noreply.github.com>
2023-03-24 08:48:42 +0530

    Add files via upload

---
+++ SeriLogDemo/SeriLogDemo/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeriLogDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().CreateLogger();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog((hostingContext, LoggerConfiguration) =>LoggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration))
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
